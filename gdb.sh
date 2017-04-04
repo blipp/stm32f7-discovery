88643bd3d8829aca229bb510e4a9c3a815d3638b
Julian Herr <mail@julianherr.de>
2017-04-04 16:53:07 +0200

    Add release switch to gdb.sh

--- gdb.sh
+++ gdb.sh
@@ -5,4 +5,15 @@ set -e
 echo "Please run 'st-util' in another terminal window (you might need sudo)"
 echo ""
 
-arm-none-eabi-gdb -iex 'add-auto-load-safe-path .' -ex "tar ext :4242" -ex "load-reset" target/stm32f7/debug/stm32f7_discovery
+case "$@" in
+
+    "--release")
+
+        binary=release
+        ;;
+    *)
+        binary=debug
+        ;;
+esac
+
+arm-none-eabi-gdb -iex 'add-auto-load-safe-path .' -ex "tar ext :4242" -ex "load-reset" target/stm32f7/"$binary"/stm32f7_discovery
